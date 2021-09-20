a7ed972696f360da2ed8be08fe77060b2ab93abf
Lucky2307 <alana.luki@gmail.com>
2021-09-20 22:40:23 +0700

    20 Sept 2021, Dictionary & lists

---
+++ 1-DictionaryLists.py
@@ -0,0 +1,9 @@
+dictionary = {
+    "bool": [True, False],
+    "what": ["bogos", "binted"],
+    "nums": [1,2,3,4]
+}
+
+names = ["John", "Jono", "Jefri", "Janear", "Jafar"]
+
+print("{},{}".format(dictionary["bool"][1], dictionary["nums"][2]))
